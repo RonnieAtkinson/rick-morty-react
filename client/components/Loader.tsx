a6b86baeb5029771afb1f4b1f5edbaf9d5586690
RonnieAtkinson <1983roncode@gmail.com>
2023-02-27 20:16:47 +0000

    Initial commit

---
+++ client/components/Loader.tsx
@@ -0,0 +1,22 @@
+// ==========================================================================
+// COMPONENTS / #LOADER
+// ==========================================================================
+
+/**
+ * Imports
+ *
+ * 1. Import react
+ *
+ */
+import React from 'react'; // [1]
+
+/**
+ * Loader component
+ *
+ * @returns {React.ReactElement} React element
+ * @example <Loader />
+ *
+ */
+export const Loader = (): React.ReactElement => {
+    return <p>Loading...</p>;
+};
