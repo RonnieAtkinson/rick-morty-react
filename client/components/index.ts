24813e2b545dfd78cdf02bf87fc9839c5c40d506
RonnieAtkinson <1983roncode@gmail.com>
2023-03-06 16:40:42 +0000

    Removed imports for components that were moved

--- client/components/index.ts
+++ client/components/index.ts
@@ -3,16 +3,10 @@
 // ==========================================================================
 
 export * from './Layout';
-export * from './Episodes';
-export * from './Episode';
 export * from './EpisodesFor';
 export * from './EpisodeList';
-export * from './Characters';
-export * from './Character';
 export * from './CharactersFor';
 export * from './CharacterList';
-export * from './Locations';
-export * from './Location';
 export * from './LocationLink';
 export * from './LocationList';
 export * from './Filter';
